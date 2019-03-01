7cbe8b7678b9dbe031caaf89d30ef8290cba1978
casa <casa@casa-PC>
2019-03-01 15:10:01 +0100

    Pongo codigo de ejercicio de pagina y añado source: generate using fields como pone en ejercicio

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -2,9 +2,28 @@ package sumador;
 
 public class ASumar {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String valor_inicial;
+	
+	public ASumar(String string) {
+        this.valor_inicial = string;
+    }
+	public String mostrar() {
+        String numero = valor_inicial;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+		return numero; 
+}
+	public int total() {
+        int suma = 0;
 
-	}
+        for (int i = 0; i < valor_inicial.length(); i++) {
+            // Cadena de un dígito.
+            String digito = valor_inicial.substring(i, 1);
+            suma = suma + Integer.getInteger(digito);
+        }
+        return suma;
+
+    }
 
 }
