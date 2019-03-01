2fb52bf6f8ac2cbf64ed1a36a66d7a15405bb70f
casa <casa@casa-PC>
2019-03-01 15:10:26 +0100

    añado funcion test de ejercicio

--- sumador/test/sumador/ASumarTest.java
+++ sumador/test/sumador/ASumarTest.java
@@ -49,5 +49,12 @@ class ASumarTest {
 	void test() {
 		fail("Not yet implemented");
 	}
+	@Test
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar("5");
+        String resultado = sumi.mostrar();
+        assertEquals("5 = 5",resultado );
+    }
+
 
 }
